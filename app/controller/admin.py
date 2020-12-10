8c85207e4fe4ca761996a9b9a03a83d7fc603c69
Madhav Parikh <parikh.madhav1999@gmail.com>
2020-12-11 00:43:20 +0530

    Minor Improvs (#2)
    Added Missing Dependencies
    Added universal error handler

--- app/controller/admin.py
+++ app/controller/admin.py
@@ -1,13 +1,10 @@
 from bson.objectid import ObjectId
 from flask import Flask, render_template, Blueprint, request, g, session, redirect, url_for
-# from passlib.hash import sha256_crypt
+from werkzeug.exceptions import HTTPException
 from datetime import datetime, timedelta
 from functools import wraps
 from flask_pymongo import PyMongo
 from bson.json_util import dumps,loads
-import sqlite3
-import random
-import string
 from .controller import *
 
 admin = Blueprint('admin',
@@ -15,6 +12,18 @@ admin = Blueprint('admin',
                  template_folder='templates',
                  static_folder='static/dash',
                  url_prefix='/admin')
+
+@admin.app_errorhandler(HTTPException)
+def handle_exception(e):
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 @admin.route('/')
 def admintest():
