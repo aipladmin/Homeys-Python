b15de5e204115a37171177be285f90690232bd3c
Madhav Parikh <parikh.madhav1999@gmail.com>
2021-01-05 09:47:51 +0530

    view PG image Code

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -63,49 +63,6 @@ def mysql_query(sql):
         return None
 
 
-# SQLITE QUERY DRIVER
-def sql_query(sql, sqldt):
-    # print("SQLDT:"+sqldt)
-    try:
-        
-        if sqldt is not None:
-            with sqlite3.connect("app.db") as con:
-                cur = con.cursor()
-                # print(sql, "        ", sqldt)
-                cur.execute(sql, sqldt)
-                if sql.split(' ')[0].lower() == "select" :
-                    rows = cur.fetchall()
-                    # print(sql, "        ", sqldt)
-                    # print("           SELECT           ")
-                    # print(rows)
-                    flag =1
-                else:
-                    # print("           NOTSELECT           ",sql)
-                    con.commit()
-                    flag=0
-
-        if sqldt is None:
-            with sqlite3.connect("app.db") as con:
-                cur = con.cursor()
-                print(sql)
-                cur.execute(sql)
-                rows = cur.fetchall()
-                flag=1
-    except con.Error as e:
-        print("Error: {}".format(e.args[0]))
-    finally:
-        con.close()
-        
-        if flag == 0:
-            # print('             SELECTFLAg               ')
-            con.close()
-            return None
-        else:
-            # print('             SELECT FLAG ROWS               ')
-            con.close()
-            return rows
-
-
 # MAIL DRIVER
 def send_mail(**deets):
     mail = Mail()
