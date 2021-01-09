f878fd553fc4fccb4e6c9de0d3ad53ff8101d69d
Trilok Sharma <strilok4031@gmail.com>
2021-01-10 01:46:09 +0530

    user side changes

--- app/controller/user.py
+++ app/controller/user.py
@@ -41,7 +41,6 @@ def handle_exception(e):
   
 @user.route('/')
 def user_dash():
-
     return render_template('user/user_dashboard.html')
 
 # @user.route('/usernd')
@@ -53,7 +52,7 @@ def pg_ads():
 
 # search Pg function Starts From here :
 
-@user.route('/Search Paying accomodation')
+@user.route('/Search_pg')
 def searchpg():
     data = mysql_query("Select pg_mst.pg_name,pg_mst.pg_gender,pg_mst.area,pg_mst.city,pg_mst.state,pg_mst.pincode,pg_mst.total_rooms,pg_mst.prop_desc,user_mst.email  from pg_mst join user_mst where utmid={}".format(2))
     # print(data)
@@ -71,14 +70,14 @@ def searchpg():
                 lst.append(y[10:])
                 dict = {'images':lst}
                 data[int(cntr)].update(dict)
-                print(data)    
+                print(data)
         #    else:
         #         # print(x)
-        #         dict = {'images':None}    
-        #         data[int(cntr)].update(dict) 
-               
-                   
-            
+        #         dict = {'images':None}
+        #         data[int(cntr)].update(dict)
+
+
+
     return render_template('user/searchpg.html',data=data)
 
 # Searh pg ends here 
