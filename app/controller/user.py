dd171fb827a363fbd096a9b0f78cab4016cd734a
Bhupender <bhupender@users.noreply.github.com>
2021-01-17 13:57:14 +0530

    search bar working dynamically

--- app/controller/user.py
+++ app/controller/user.py
@@ -9,55 +9,83 @@ import string
 from .controller import *
  
 user = Blueprint('user',
-                __name__,
-                url_prefix='/user',
-                template_folder='templates',
-               static_folder='static/auth')
+				__name__,
+				url_prefix='/user',
+				template_folder='templates',
+			   static_folder='static/auth')
 
 @user.app_errorhandler(HTTPException)
 def handle_exception(e):
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
-    response.content_type = "application/json"
-    return response
+	response = e.get_response()
+	# replace the body with JSON
+	response.data = json.dumps({
+		"code": e.code,
+		"name": e.name,
+		"description": e.description,
+	})
+	response.content_type = "application/json"
+	return response
 
 @user.app_errorhandler(HTTPException)
 def handle_exception(e):
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
-    response.content_type = "application/json"
-    return response
+	response = e.get_response()
+	# replace the body with JSON
+	response.data = json.dumps({
+		"code": e.code,
+		"name": e.name,
+		"description": e.description,
+	})
+	response.content_type = "application/json"
+	return response
 
 @user.route('/')
 @user.route('/pg_ads')
 def pg_ads():
-    data = mysql_query("Select pg_mst.pg_name,pg_mst.addr_1,pg_mst.addr_2,pg_mst.pg_gender,pg_mst.area,pg_mst.city,pg_mst.state,pg_mst.pincode,pg_mst.total_rooms,pg_mst.prop_desc,user_mst.email from pg_mst inner join user_mst on pg_mst.uid=user_mst.uid")
-    file_names = get_file_list_s3(bucket='mittrisem', prefix='pg_images/')
-    # print(file_names)
-    cntr = -1
-    for x in data:
-        cntr = cntr + 1
-        length = x['email'] + '_' + x['pg_name']
-        xLen = len(length)
-        lst = []
-        for y in file_names:
-            if y[10:int(xLen + 10)] == x['email'] + '_' + x['pg_name']:
-                print(y, cntr)
-                lst.append(y[10:])
-                dict = {'images': lst}
-                data[int(cntr)].update(dict)
-            print(data)
+	data = mysql_query("Select pg_mst.pg_name,pg_mst.addr_1,pg_mst.addr_2,pg_mst.pg_gender,pg_mst.area,pg_mst.city,pg_mst.state,pg_mst.pincode,pg_mst.total_rooms,pg_mst.prop_desc,user_mst.email,GROUP_CONCAT(facility_mst.amenity) as facilities from pg_mst inner join user_mst on pg_mst.uid=user_mst.uid inner join facility_mst on facility_mst.pgid=pg_mst.pgid group by pg_mst.pgid")
+	file_names = get_file_list_s3(bucket='mittrisem', prefix='pg_images/')
+	# print(file_names)
+	cntr = -1
+	for x in data:
+		cntr = cntr + 1
+		length = x['email'] + '_' + x['pg_name']
+		xLen = len(length)
+		lst = []
+		for y in file_names:
+			if y[10:int(xLen + 10)] == x['email'] + '_' + x['pg_name']:
+				print(y, cntr)
+				lst.append(y[10:])
+				dict = {'images': lst}
+				data[int(cntr)].update(dict)
+			print(data)
 
-    return render_template('user/pg_ads.html',data=data)
+	return render_template('user/pg_ads.html',data=data)
 
 
+@user.route('/search',methods=['POST','GET'])
+def search():
+	if request.method == "POST":
+		if "button1" in request.form:
+			pgname=request.form['byname']
+			gen=request.form['sel1']
+			area=request.form['byarea']
+			data = mysql_query("Select user_mst.email,pg_mst.pgid,pg_mst.pg_name,pg_mst.addr_1,pg_mst.addr_2,pg_mst.pg_gender,pg_mst.area,pg_mst.city,pg_mst.state,pg_mst.pincode,pg_mst.total_rooms,pg_mst.prop_desc, GROUP_CONCAT(facility_mst.amenity) as facilities from pg_mst inner join user_mst on pg_mst.uid=user_mst.uid inner join facility_mst on facility_mst.pgid=pg_mst.pgid group by pg_mst.pgid having pg_mst.pg_name='{}' or pg_mst.pg_gender='{}' or pg_mst.area='{}' ".format(pgname,gen,area))
+			for x in data:
+				print(x['pgid'])
+			file_names = get_file_list_s3(bucket='mittrisem', prefix='pg_images/')
+			# print(file_names)
+			cntr = -1
+			for x in data:
+				cntr = cntr + 1
+				length = x['email'] + '_' + x['pg_name']
+				xLen = len(length)
+				lst = []
+				for y in file_names:
+					if y[10:int(xLen + 10)] == x['email'] + '_' + x['pg_name']:
+						print(y, cntr)
+						lst.append(y[10:])
+						dict = {'images': lst}
+						data[int(cntr)].update(dict)
+					
+			print(data)	
+			return render_template('user/pg_ads.html',data=data)
+	return render_template('user/pg_ads.html')
